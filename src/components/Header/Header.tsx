243a86f6877eae6344ae9267147f14b3f2ca6adf
Ester <helo.esteralmeida@gmail.com>
2026-03-17 22:34:40 -0300

    Home e Menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,16 @@
 import { Logo } from "../Header/Logo"
+import { Menu } from "../Header/Menu"
 
 export function Header() {
     return (
         <header className="bg-purple-950 py-4 text-white">
-            <div className="container mx-auto flex">
+            <div className="container mx-auto flex justify-between items-center">
                 <Logo/>
+
+                <Menu/>
             </div>
 
-            <div></div>
+          
         </header>
     );
 }
---
+++ src/components/Header/Menu.tsx
@@ -0,0 +1,11 @@
+export function Menu(){
+    return (
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                 <li><a href="/">Login</a></li>
+                  <li><a href="/">cadastro</a></li>
+            </ul>
+        </nav>
+    )
+}
