730666f629418f15245d7757a24782ffa02af0bb
Ester-creater <helo.esteralmeida@gmail.com>
2026-03-31 22:28:47 -0300

    30-01

--- src/components/EditPage.tsx
+++ src/components/EditPage.tsx
@@ -1,14 +1,37 @@
 import { useState } from "react"
 import { Input } from "./Input"
 
+interface Link {
+    name: string,
+    url: string
+}
+
 export const EditPage = () => {
     const [name, setName] = useState("Stellar");
+    const [Links, setLinks] = useState<Link[]>([]);
+
+    const handleAddLink = () => {
+        setLinks([...Links, {name: "", url: ""}]);
+    }
+
+    const handleLinkChange = (index: number, field: string, value: string) => {
+        const updatedLinks = [...Links];
+
+        updatedLinks[index] = {
+            ...updatedLinks[index], 
+            [field]: value,
+        }
+
+        setLinks(updatedLinks);
+    }
 
     return (
         <div>
             <div className="container mx-auto flex ">
                 <main className="w-8/12 bg-purple-600 p-4">
-                    <form className="space-y-4">
+                    <form className="space-y-4" onSubmit={(event) => {
+                        event.preventDefault();
+                    }}>
 
                         <Input 
                         name="Nome" 
@@ -22,9 +45,9 @@ export const EditPage = () => {
                         <Input name="GitHub" 
                         id="github" 
                         placeholder="Ex: ester-creater" 
-                        value={name} 
+                        value={""} 
                         onChange={(event) => {
-                            setName(event.target.value);
+                    
                         }}/>
 
                         <div className="grid grid-cols-4 gap-4">
@@ -33,11 +56,45 @@ export const EditPage = () => {
                         <Input name="TikTok" id="tiktok" placeholder="Ex: ester-creater"/>
                         <Input name="Facebook" id="facebook" placeholder="Ex: ester-creater"/>
                         </div>
+
+                        <div className="space-y-4">
+                            {
+                                Links.map((link, index) => {
+                                    return (
+                                        <div className="flex gap-4">
+                                            <Input
+                                                name={"Link " + index + 1}
+                                                id={"Link-" + index}
+                                                value={link.name}
+                                                placeholder="Ex: Portfólio"
+                                                onChange={(event) => {
+                                                    handleLinkChange(index, "name", event.target.value);
+                                                }}
+                                            />
+                                            <Input
+                                                name={"URL do Link"}
+                                                id={"link-url-" + index}
+                                                value={link.url}
+                                                placeholder="Ex: https://meusite.com"
+                                                onChange={(event) => {
+                                                    handleLinkChange(index, "url", event.target.value)
+                                                }}
+                                            />
+                                        </div>     
+                                    )
+
+                                })
+                            }
+                            <button className="bg-white rounded cursor-pointer uppercase px-4 py-2 
+                            hover:opacity-90 transition-opacity" 
+                            onClick={handleAddLink}>Adicionar Link</button>
+                        </div>
+
                     </form>
                 </main>
 
                 <aside className="w-4/12 bg-purple-300">
-                    Seu nome: {}
+                    Seu nome: {name}
                 </aside>
             </div>
         </div>
